72cfbdb8abfffb6a015bb2f08137cd5f9fc4a6df
Shivamsingh777 <shivamsingh271310@gmail.com>
2025-07-31 23:02:45 +0530

    cors issue

--- devconnect-backend/server.js
+++ devconnect-backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://preeminent-caramel-e9942a.netlify.app"],
     credentials: true,
   })
 );app.use(express.json());
